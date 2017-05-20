e312272468c7f9fe2f90cef40c4932873581df85
Никита <JacksonGibsonESP@gmail.com>
2017-05-20 21:32:18 +0300

    КР 3

--- test3/index.js
+++ test3/index.js
@@ -1,11 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static('static'));
 app.use('/angular', express.static('node_modules/angular'));
+app.use(bodyParser.json());
 app.post('/', function(req, res){
-    res.write('');
+    res.write("success");
     res.end();
+    console.log(req.body.name);
+    console.log(req.body.email);
 });
 app.listen(8080, function(){
     
